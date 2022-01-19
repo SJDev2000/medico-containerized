5fe754bc8fc504ac54cca244bec3441ac346e673
SaiyamJain <saiyamjain3000@gmail.com>
2022-01-19 21:14:52 +0530

    Updated Containers

---
+++ web/flaskproject/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.secret_key = "mysecretkey123"
+app.config["SQLALCHEMY_DATABASE_URI"] = "postgresql://admin:admin@db:5432/medico"
+app.config["SQLALCHEMY_TRACK_MODIFICATION"] = False
+db = SQLAlchemy(app)
+
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+UPLOAD_FOLDER = './static/uploads'
+app.config["UPLOAD_FOLDER"] = UPLOAD_FOLDER
+
+from flaskproject import routes
